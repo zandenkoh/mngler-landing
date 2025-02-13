eccb25eb59995796ffb8131b47ba70147c4a33a7
Zanden Koh <137964632+zandenkoh@users.noreply.github.com>
2025-02-13 21:02:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ const urlsToCache = [
   "/index.html",
   "/styles.css",
   "/script.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/Mngler-Landing.png"
 ];
 
 self.addEventListener("install", (event) => {
